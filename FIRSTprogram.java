bc7f8156ef0f06890b9fc21059986a647ef6735b
Mhanzala2k24 <hanzalamuhammad661@gmail.com>
2025-01-04 05:28:39 -0800

    Java loop

---
+++ FIRSTprogram.java
@@ -0,0 +1,12 @@
+public class FIRSTprogram {
+
+
+public static void main(String[] args) {
+    for (int i = 0; i <=20; i++ ){
+        System.out.println("PAKISTAN  ZINDABAD");
+    }
+    for (int b = 0; b<11; b+=2){
+        System.out.println(b);
+    }
+}
+}
