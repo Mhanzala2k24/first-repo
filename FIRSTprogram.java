d405d3e60270d6eb1795e2d93bd9ea833e15c77f
Mhanzala2k24 <hanzalamuhammad661@gmail.com>
2025-01-04 06:24:05 -0800

    JAVA LOOP

--- FIRSTprogram.java
+++ FIRSTprogram.java
@@ -1,12 +1,51 @@
+import java.util.Scanner;
+
 public class FIRSTprogram {
 
 
 public static void main(String[] args) {
-    for (int i = 0; i <=20; i++ ){
-        System.out.println("PAKISTAN  ZINDABAD");
+    Sum();
+    // for (int i = 0; i <=20; i++ ){
+    //     System.out.println("PAKISTAN  ZINDABAD");
+    // }
+    // for (int b = 0; b<11; b+=2){
+    //     System.out.println(b);
+    // }
+
+    // int c = 0;
+    // while(c <= 10){
+    //     System.out.println(c);
+    //     c++;
+    // }
+
+    // int d = 0;
+    // do{
+    //     System.out.println(d);
+    //     d+= 10;
+    // }while(d <=1000);
+
+ //  int a = 12;
+    // while (a < 11){
+    //     System.out.println("PAKISTAN ZINDABAD!");
+    //     a++;
+    // }
+
+    //  do{
+    //      System.out.println("PAKISTAN ZINDABAD");
+    //      a += 2;
+    //  }while (a <=11);
     }
-    for (int b = 0; b<11; b+=2){
-        System.out.println(b);
-    }
-}
+     public static void Sum(){
+        Scanner input = new Scanner (System.in);
+        System.out.println("ENTER THE NUMBER: ");
+        int f = input.nextInt();
+
+        int i = 0;
+        while(i <= f){
+            System.out.println(i);
+            i += 2;
+        }
+
+     }
+
 }
