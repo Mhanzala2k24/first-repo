3d5fab695eef3ac6167ac889e97366215f47e5a0
Mhanzala2k24 <hanzalamuhammad661@gmail.com>
2025-01-28 08:31:59 -0800

    CONSTRUCTOR

---
+++ Car.java
@@ -0,0 +1,14 @@
+public class Car {
+    
+String color;
+int model;
+
+public Car(String color,int model){
+    this.color = color;
+    this.model = model;
+}
+ void display(){
+    System.out.println("CAR COLOR: "+color+"\nCAR MODEL: "+model);
+}
+
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+
+
+    public static void main(String[] args) {
+        Car car1 = new Car("black",2019);
+        Car car2 = new Car("grey",2020);
+
+
+car1.display();
+car2.display();
+    }
+}
